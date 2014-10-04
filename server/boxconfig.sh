4b56e0affffe1048eacd53bb62c164f61182cb07
coldclimate <oliver@coldclimate.co.uk>
2014-10-04 17:27:32 +0100

    fuck you ruby

--- server/boxconfig.sh
+++ server/boxconfig.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
-echo "deb http://archive.canonical.com/ quantal partner" | sudo tee -a /etc/apt/sources.list
-echo "deb http://nginx.org/packages/ubuntu/ quantal nginx" | sudo tee -a /etc/apt/sources.list
-echo "deb-src http://nginx.org/packages/ubuntu/ quantal nginx" | sudo tee -a /etc/apt/sources.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv ABF5BD827BD9BF62
 sudo apt-get update
-sudo apt-get -y install git nginx ruby ruby-dev   rubygems build-essential
-sudo gem install bundler
-bundle install
-sudo cp /vagrant/server/default.conf /etc/nginx/conf.d
-sudo service nginx restart
-
+sudo apt-get -y install git ruby ruby-dev rubygems build-essential
+sudo gem install nanoc
+sudo gem install adsf
+sudo gem install kramdown
+sudo gem install nanoc-cachebuster
